24ada8b3a4b494a08f190a6cfad3e388a5053f0f
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-01-27 15:45:59 +0100

    Script for creating users table

--- mysql/init.sql
+++ mysql/init.sql
@@ -2,4 +2,11 @@ CREATE DATABASE IF NOT EXISTS stackoverflow_clone_dev;
 
 USE stackoverflow_clone_dev;
 
--- CREATE TABLE IF NOT EXISTS (...);
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password BINARY(60) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
